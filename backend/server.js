66e27df13717bd7305938a55bb8db6f2663e2d9a
DilipSingh1008 <diliprjprht@gmail.com>
2025-05-05 22:14:23 +0530

    Changed port number in server.js

--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,7 @@ const authRoute = require("./router/auth-router.js");
 const contactRoute = require('./router/contact-router.js')
 const errorMiddleware = require('./middlewares/error-middleware.js')
 const adminRoute = require('./router/admin-route.js')
-const PORT = process.env.PORT || 10000;
+const PORT = process.env.PORT || 5000;
 
 // app.use(cors());
 app.use(express.json());
@@ -26,8 +26,8 @@ app.use('/api/admin/',adminRoute)
 app.use(errorMiddleware)
 
 connectDb().then(()=>{
-  app.listen(PORT, () => {
-    console.log(`🚀 Server running on http://localhost:${PORT}`);
+  app.listen(PORT, '0.0.0.0', () => {
+    console.log(`🚀 Server running on http://0.0.0.0:${PORT}`);
   });
 }).catch((err)=>{
   console.log("server is not connect");
