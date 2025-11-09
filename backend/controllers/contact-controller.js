65316ebf7a5a3ba998ce45a0d28ecb89fcb279fa
DilipSingh1008 <diliprjprht@gmail.com>
2025-11-09 21:53:43 +0530

    add

--- backend/controllers/contact-controller.js
+++ backend/controllers/contact-controller.js
@@ -3,47 +3,39 @@ const Contact = require('../model/contact-model')
 
 
 const contactForm = async (req, res) => {
-     try{
-          const resposne = req.body
-        
-          await Contact.create(resposne);
+    try {
+        const resposne = req.body
+         await Contact.create(resposne)
 
-          const transporter = nodemailer.createTransport({
+        const transporter = nodemailer.createTransport({
             service: "gmail",
             auth: {
-                user: process.env.GMAIL_USER,  // e.g. "yourmail@gmail.com"
-                pass: process.env.GMAIL_PASS,  // App password
+                user: process.env.GMAIL_USER,  
+                pass: process.env.GMAIL_PASS,  
             },
         });
-        // console.log(transporter)
-        
 
-        // 3️⃣ Define mail content
+
         const mailOptions = {
-            from: process.env.GMAIL_USER,     // Use your Gmail as sender
-      to: process.env.GMAIL_USER, 
+            from: resposne.email, 
+            to: process.env.GMAIL_USER, 
             subject: `New Contact Form Submission from ${resposne.username}`,
             text: `
-Name: ${resposne.username}
-Email: ${resposne.email}
-Message: ${resposne.message}
-      `,
-            replyTo: resposne.email,          // User can reply
+            Name: ${resposne.username}
+            Email: ${resposne.email}
+            Message: ${resposne.message}
+               `,
+            };
 
-        };
-
-        // 4️⃣ Send email
         await transporter.sendMail(mailOptions);
 
         return res.status(200).json({ message: "Message sent & email delivered" });
+        // return res.status(200).json({ message: "message send succesfully " })
 
-    } catch(error){
-         
-console.error("❌ Error while saving contact:", error);
-  return res.status(400).json({ 
-    message: "message not deliverd", 
-    error: error.message 
-  });    }
+    } catch (error) {
 
+        return res.status(400).json({ message: "message not deliverd" })
+    }
 }
+
 module.exports = contactForm
