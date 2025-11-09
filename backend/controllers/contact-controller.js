1d25b8b09044b8689362555cba690f58a3fe34e8
DilipSingh1008 <diliprjprht@gmail.com>
2025-11-09 22:19:33 +0530

    host add

--- backend/controllers/contact-controller.js
+++ backend/controllers/contact-controller.js
@@ -5,27 +5,33 @@ const Contact = require('../model/contact-model')
 const contactForm = async (req, res) => {
     try {
         const resposne = req.body
-         await Contact.create(resposne)
+        await Contact.create(resposne)
 
         const transporter = nodemailer.createTransport({
-            service: "gmail",
+            host: "smtp.gmail.com",
+            port: 465,          // or 587 for TLS
+            secure: true,
             auth: {
-                user: process.env.GMAIL_USER,  
-                pass: process.env.GMAIL_PASS,  
+                user: process.env.GMAIL_USER,
+                pass: process.env.GMAIL_PASS,
             },
         });
 
+        await transporter.verify();
+        console.log("SMTP server is ready to take messages");
+
 
         const mailOptions = {
-             from: process.env.GMAIL_USER, 
-            to: process.env.GMAIL_USER, 
+            from: process.env.GMAIL_USER,
+            to: process.env.GMAIL_USER,
+            replyTo: response.email,
             subject: `New Contact Form Submission from ${resposne.username}`,
             text: `
             Name: ${resposne.username}
             Email: ${resposne.email}
             Message: ${resposne.message}
                `,
-            };
+        };
 
         await transporter.sendMail(mailOptions);
 
@@ -33,9 +39,9 @@ const contactForm = async (req, res) => {
         // return res.status(200).json({ message: "message send succesfully " })
 
     } catch (error) {
-    console.error("Error sending email:", error);
-    return res.status(400).json({ message: "Message not delivered", error: error.message });
-}
+        console.error("Error sending email:", error);
+        return res.status(400).json({ message: "Message not delivered", error: error.message });
+    }
 
 }
 
